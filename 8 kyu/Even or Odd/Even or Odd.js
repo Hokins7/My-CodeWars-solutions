f57293dfaded69c32eff7b9d4b0a35fa0a03fa1e
25hokins25 <25hokins25@gmail.com>
2023-10-02 08:50:58 -0700

    Add solution

---
+++ 8 kyu/Even or Odd/Even or Odd.js
@@ -0,0 +1,9 @@
+/**
+ * Determine if a number is even or odd.
+ * @param {number} number - The number to be checked.
+ * @returns {string} - 'Even' if the number is even, 'Odd' if it's odd.
+ */
+function even_or_odd(number) {
+    return number % 2 === 0 ? 'Even' : 'Odd';
+  }
+  
