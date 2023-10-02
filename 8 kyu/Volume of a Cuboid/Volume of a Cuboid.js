90376cd5c76a0bd6a215f6e8d361c880d1b72760
25hokins25 <25hokins25@gmail.com>
2023-10-02 08:45:20 -0700

    Add solution, task and readme

---
+++ 8 kyu/Volume of a Cuboid/Volume of a Cuboid.js
@@ -0,0 +1,15 @@
+/**
+ * Calculate the volume of a cuboid (rectangular box) using its length, width, and height.
+ */
+class Kata {
+  /**
+   * Get the volume of the cuboid.
+   * @param {number} length - The length of the cuboid.
+   * @param {number} width - The width of the cuboid.
+   * @param {number} height - The height of the cuboid.
+   * @returns {number} - The volume of the cuboid.
+   */
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
+  }
+}
