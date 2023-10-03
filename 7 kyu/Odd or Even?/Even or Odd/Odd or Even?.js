f838e48c937a2503c6a45fcba3097a0c165f421c
25hokins25 <25hokins25@gmail.com>
2023-10-03 09:16:06 -0700

    Finish solution

--- 7 kyu/Odd or Even?/Even or Odd/Odd or Even?.js
+++ 7 kyu/Odd or Even?/Even or Odd/Odd or Even?.js
@@ -1,9 +1,14 @@
 /**
- * Determine if a number is even or odd.
- * @param {number} number - The number to be checked.
- * @returns {string} - 'Even' if the number is even, 'Odd' if it's odd.
+ * Determine if the sum of a list of integers is odd or even.
+ * @param {number[]} array - An array of integers.
+ * @returns {string} - 'even' if the sum is even, 'odd' if it's odd.
  */
-function even_or_odd(number) {
-    return number % 2 === 0 ? 'Even' : 'Odd';
+function oddOrEven(array) {
+    // Use the reduce() method to calculate the sum of the integers in the array.
+    let sum = array.reduce((partialSum, a) => partialSum + a, 0);
+    
+    // Check if the sum is even or odd using the modulo operator (%).
+    if (sum % 2 === 0) return "even";
+    else return "odd";
   }
   
