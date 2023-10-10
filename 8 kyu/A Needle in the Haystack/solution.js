152c109824f0ee8b422e105ed4ddc151770ff94a
25hokins25 <25hokins25@gmail.com>
2023-10-10 09:47:24 -0700

    Add solution

---
+++ 8 kyu/A Needle in the Haystack/solution.js
@@ -0,0 +1,10 @@
+/**
+ * This JavaScript code defines a function 'findNeedle' that searches for the word "needle" in an array and returns a message.
+ *
+ * @param {string[]} haystack - The input array containing the words.
+ * @returns {string} - A message indicating the position where "needle" was found.
+ */
+function findNeedle(haystack) {
+    return "found the needle at position " + haystack.indexOf("needle");
+  }
+  
