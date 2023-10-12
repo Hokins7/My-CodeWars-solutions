7b997620efe4bc2702383208239316f3572a585d
25hokins25 <25hokins25@gmail.com>
2023-10-12 08:51:59 -0700

    Add solution

---
+++ 8 kyu/Beginner - Reduce but Grow/solution.js
@@ -0,0 +1,10 @@
+/**
+ * This JavaScript code defines a function 'maps' that takes an array of integers and returns a new array with each value doubled.
+ *
+ * @param {number[]} x - The input array of integers.
+ * @returns {number[]} - A new array where each value is doubled.
+ */
+function maps(x) {
+    return x.map(v => v * 2);
+  }
+  
