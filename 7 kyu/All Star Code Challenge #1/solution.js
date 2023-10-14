2f05f7ecc0fe6fbc77bab5b14b86b1261c8b9f10
25hokins25 <25hokins25@gmail.com>
2023-10-14 12:28:35 -0700

    Add solution

---
+++ 7 kyu/All Star Code Challenge #1/solution.js
@@ -0,0 +1,11 @@
+/**
+ * This JavaScript code defines a function 'sumPPG' that takes two NBA player objects and calculates the sum of their Points Per Game (PPG).
+ *
+ * @param {Object} playerOne - An NBA player object with 'ppg' property.
+ * @param {Object} playerTwo - Another NBA player object with 'ppg' property.
+ * @returns {number} - The sum of PPG for the two players.
+ */
+function sumPPG(playerOne, playerTwo) {
+    return playerOne.ppg + playerTwo.ppg;
+  }
+  
