d10492e7982b455c877c7a15a90225f8c5c9d823
25hokins25 <25hokins25@gmail.com>
2023-10-18 08:43:03 -0700

    Add solution

---
+++ 8 kyu/Chuck Norris VII - True or False? (Beginner)/solution.js
@@ -0,0 +1,12 @@
+/**
+ * This JavaScript code defines a function 'ifChuckSaysSo' that returns false.
+ * It achieves this by using a logical NOT operator (!) on the number 2.
+ * In JavaScript, the logical NOT operator negates a truthy value to false.
+ * Since 2 is a truthy value, applying the NOT operator on it results in false.
+ *
+ * @returns {boolean} - Always returns false.
+ */
+function ifChuckSaysSo() {
+    return !2;
+  }
+  
