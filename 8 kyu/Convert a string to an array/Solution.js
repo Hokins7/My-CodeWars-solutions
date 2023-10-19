676359fcc15f1eaca4a80236bc85986bb0425c3a
25hokins25 <25hokins25@gmail.com>
2023-10-19 08:28:41 -0700

    Add solution

---
+++ 8 kyu/Convert a string to an array/Solution.js
@@ -0,0 +1,12 @@
+/**
+ * This JavaScript code defines a function 'stringToArray' that splits a string into an array of words.
+ *
+ * @param {string} string - The input string to be split into words.
+ * @returns {array} - An array of words extracted from the input string.
+ */
+function stringToArray(string) {
+    // We will use the `split` method with a space (' ') as the delimiter to split the string into an array of words.
+    return string.split(' ');
+  }
+  
+  
