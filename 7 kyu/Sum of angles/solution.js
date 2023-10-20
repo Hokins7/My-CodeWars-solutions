f57c880436bab42de0c5fcd33dc81d05d1b2854f
25hokins25 <25hokins25@gmail.com>
2023-10-20 08:50:54 -0700

    Add solution

---
+++ 7 kyu/Sum of angles/solution.js
@@ -0,0 +1,10 @@
+/**
+ * This JavaScript code defines a function 'angle' that calculates the sum of internal angles of an n-sided polygon.
+ *
+ * @param {number} n - The number of sides in the polygon.
+ * @returns {number} - The total sum of internal angles in degrees.
+ */
+function angle(n) {
+    return (n - 2) * 180;
+  }
+  
