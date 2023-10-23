03044d0a9b50bed84c625a7fa49004f5fb422465
25hokins25 <25hokins25@gmail.com>
2023-10-23 09:21:35 -0700

    Add solution

---
+++ 8 kyu/Count Odd Numbers below n/solution.js
@@ -0,0 +1,10 @@
+/**
+ * This JavaScript code defines a function 'oddCount' that calculates the number of positive odd numbers below a given number.
+ *
+ * @param {number} n - The input number.
+ * @returns {number} - The count of positive odd numbers below the input number.
+ */
+function oddCount(n) {
+    return Math.floor(n / 2);
+  }
+  
