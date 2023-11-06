49b95e37f20f435c8709e2354ce13e55eb227264
25hokins25 <25hokins25@gmail.com>
2023-11-06 15:26:22 -0800

    Add solution

---
+++ 8 kyu/Get Nth Even Number/solution.js
@@ -0,0 +1,6 @@
+/**
+ * This function, nthEven, returns the Nth even number.
+ * @param {number} n - The index of the even number to be returned.
+ * @returns {number} - The Nth even number.
+ */
+const nthEven = n => n * 2 - 2;
