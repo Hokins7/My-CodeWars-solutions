e584f7d6251f72a7931be6dd72eb249315b485c8
25hokins25 <25hokins25@gmail.com>
2023-11-11 15:52:13 -0800

    Add solution

---
+++ 8 kyu/Grasshopper - Basic Function Fixer/solution.js
@@ -0,0 +1,12 @@
+/**
+ * This function, addFive, adds five to the given number and returns the result.
+ * @param {number} num - The input number to which five will be added.
+ * @returns {number} - The result of adding five to the input number.
+ */
+function addFive(num) {
+    // Adding five to the input number
+    var total = num + 5;
+    // Returning the result
+    return total;
+  }
+  
